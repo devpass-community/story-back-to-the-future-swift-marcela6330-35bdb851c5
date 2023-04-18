ece243b82261aaf76ebf9cc1faabe3c106891a5c
Marcela Gonçalves <marcelasouzagoncalves@gmail.com>
2023-04-17 21:08:36 -0300

    Formating date

--- BackToTheFutureApp/Extensions/Date+Extensions.swift
+++ BackToTheFutureApp/Extensions/Date+Extensions.swift
@@ -3,14 +3,14 @@ import Foundation
 extension Date {
     
     func dateAfter(years: Int) -> Date? {
-     
+        
         // TODO
         return nil
     }
     
     func formattedDate() -> String {
-        
-        // TODO
-        return ""
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = "dd/MM/yyyy"
+        return dateformat.string(from: self)
     }
 }
